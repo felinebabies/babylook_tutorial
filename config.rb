43f36b9e2e7af7dedb1324989ba520658bf6b594
syuji <commentonly.is@gmail.com>
2014-12-25 23:29:48 +0900

    デプロイ設定を追加した。

--- config.rb
+++ config.rb
@@ -70,3 +70,10 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+# デプロイの設定
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+  deploy.branch = 'gh-pages'
+end
